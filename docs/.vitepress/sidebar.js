b9727f0662006c7e55e6ba95f2e92d2dc156d8b3
lebronxiong <lebronxiong@tencent.com>
2024-10-28 17:17:08 +0800

    feat: 自动生成目录

--- docs/.vitepress/sidebar.js
+++ docs/.vitepress/sidebar.js
@@ -1,36 +1,127 @@
-export const sidebar = [
+// !!!由代码自动生成，无需更改。
+ export const sidebar = [
   {
-    text: "浏览器原理",
-    items: [
+    "originText": "jsvascript",
+    "text": "jsvascript",
+    "collapsible": true,
+    "collapsed": true,
+    "items": [
       {
-        text: "事件循环",
-        link: "/blogs/browser/event-loop",
-      },
-      {
-        text: "fisrt",
-        link: "/blogs/blog1/first",
-      },
-      {
-        text: "second",
-        link: "/blogs/blog1/second",
-      },
-    ],
+        "text": "代码漏洞",
+        "link": "/blogs/jsvascript/代码漏洞"
+      }
+    ]
   },
   {
-    text: "blog2",
-    items: [
+    "originText": "browser",
+    "text": "浏览器",
+    "collapsible": true,
+    "collapsed": true,
+    "items": [
       {
-        text: "index",
-        link: "/blogs/blog2/",
+        "text": "event-loop",
+        "link": "/blogs/browser/event-loop"
       },
       {
-        text: "first",
-        link: "/blogs/blog2/first",
+        "text": "second",
+        "link": "/blogs/browser/second"
       },
       {
-        text: "second",
-        link: "/blogs/blog2/second",
+        "text": "浏览器渲染原理和性能优化",
+        "link": "/blogs/browser/浏览器渲染原理和性能优化"
       },
-    ],
+      {
+        "text": "浏览器随意记录",
+        "link": "/blogs/browser/浏览器随意记录"
+      }
+    ]
   },
+  {
+    "originText": "redux",
+    "text": "redux",
+    "collapsible": true,
+    "collapsed": true,
+    "items": [
+      {
+        "text": "redux原理",
+        "link": "/blogs/redux/redux原理"
+      },
+      {
+        "text": "redux思想",
+        "link": "/blogs/redux/redux思想"
+      }
+    ]
+  },
+  {
+    "originText": "performance",
+    "text": "performance",
+    "collapsible": true,
+    "collapsed": true,
+    "items": [
+      {
+        "text": "first",
+        "link": "/blogs/performance/first"
+      },
+      {
+        "text": "index",
+        "link": "/blogs/performance/index"
+      },
+      {
+        "text": "second",
+        "link": "/blogs/performance/second"
+      }
+    ]
+  },
+  {
+    "originText": "webview",
+    "text": "webview",
+    "collapsible": true,
+    "collapsed": true,
+    "items": [
+      {
+        "text": "jsapi和离线包",
+        "link": "/blogs/webview/jsapi和离线包"
+      }
+    ]
+  },
+  {
+    "originText": "mobile",
+    "text": "移动端",
+    "collapsible": true,
+    "collapsed": true,
+    "items": [
+      {
+        "text": "移动端适配",
+        "link": "/blogs/mobile/移动端适配"
+      }
+    ]
+  },
+  {
+    "originText": "thinking",
+    "text": "个人思考",
+    "collapsible": true,
+    "collapsed": true,
+    "items": [
+      {
+        "text": "中间件",
+        "link": "/blogs/thinking/中间件"
+      },
+      {
+        "text": "函数编程范式",
+        "link": "/blogs/thinking/函数编程范式"
+      }
+    ]
+  },
+  {
+    "originText": "others",
+    "text": "其他",
+    "collapsible": true,
+    "collapsed": true,
+    "items": [
+      {
+        "text": "tinyproxy搭建私人代理服务器",
+        "link": "/blogs/others/tinyproxy搭建私人代理服务器"
+      }
+    ]
+  }
 ];
--- scripts/generate-nav.js
+++ scripts/generate-nav.js
@@ -1,3 +1,89 @@
 /**
  * 自动生成docs目录下的内容
  */
+const textMap = {
+  browser: "浏览器",
+  jsvascript: "jsvascript",
+  mobile: "移动端",
+  others: "其他",
+  performance: "performance",
+  redux: "redux",
+  thinking: "个人思考",
+  webview: "webview",
+};
+
+// 自定义排序配置
+const customOrder = [
+  'jsvascript',
+  'browser',
+  'redux',
+  'performance',
+  'webview',
+  'mobile',
+  'thinking',
+  'others',
+];
+
+
+const fs = require("fs");
+const path = require("path");
+
+const blogsDir = path.join(__dirname, "../docs", "blogs");
+const sidebarFilePath = path.join(__dirname, '../docs', '.vitepress', 'sidebar.js');
+
+function getBlogStructure(dir) {
+  const result = [];
+
+  // 读取目录
+  const folders = fs
+    .readdirSync(dir, { withFileTypes: true })
+    .filter((item) => item.isDirectory());
+
+  for (const folder of folders) {
+    const folderPath = path.join(dir, folder.name);
+    const items = [];
+
+    // 读取文件夹中的文件
+    const files = fs
+      .readdirSync(folderPath, { withFileTypes: true })
+      .filter((item) => item.isFile() && item.name.endsWith(".md"));
+
+    for (const file of files) {
+      const fileName = file.name.replace(".md", ""); // 去掉 .md 后缀
+      const link = path
+        .join("/blogs", folder.name, fileName)
+        .replace(/\\/g, "/"); // 生成相对路径
+      items.push({
+        text: fileName,
+        link: link,
+      });
+    }
+
+    // 将文件夹信息添加到结果数组中
+    result.push({
+      originText: folder.name, // 原始路径名称
+      text: textMap[folder.name] || folder.name,
+      collapsible: true,
+      collapsed: true,
+      items: items,
+    });
+  }
+
+  // 根据自定义排序配置对结果进行排序
+  result.sort((a, b) => {
+    const indexA = customOrder.indexOf(a.originText);
+    const indexB = customOrder.indexOf(b.originText);
+    
+    // 如果在 customOrder 中找不到，默认放在最后
+    return (indexA === -1 ? customOrder.length : indexA) - (indexB === -1 ? customOrder.length : indexB);
+  });
+
+  return result;
+}
+
+const blogStructure = getBlogStructure(blogsDir);
+
+// 将结果写入到 sidebar.js 文件
+const sidebarContent = `// !!!由代码自动生成，无需更改。\n export const sidebar = ${JSON.stringify(blogStructure, null, 2)};\n`;
+
+fs.writeFileSync(sidebarFilePath, sidebarContent, 'utf8');
